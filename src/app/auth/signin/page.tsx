da51cd977404569a1ce92d0f77ce55e574f97e20
jeong922 <hwajeong922@naver.com>
2023-07-31 18:12:19 +0900

    로그인 페이지 justify-center 삭제

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -21,7 +21,7 @@ export default async function SigninPage({
   const providers = (await getProviders()) ?? {};
 
   return (
-    <div className='flex flex-col items-center justify-center w-full px-2 pt-32'>
+    <div className='flex flex-col items-center w-full px-2 pt-32'>
       <h2 className='w-full pb-3 mb-3 text-2xl text-center'>로그인</h2>
       <Signin providers={providers} callbackUrl={callbackUrl ?? '/'} />
     </div>
