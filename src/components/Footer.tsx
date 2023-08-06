59068bab3930d0a0b64d9205fb3f29c6aa6f6df5
jeong922 <hwajeong922@naver.com>
2023-08-06 16:32:35 +0900

    footer 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,22 +3,22 @@ import GithubIcon from './icon/GithubIcon';
 
 export default function Footer() {
   return (
-    <footer className='relative py-3 transform translate-y-full bg-gray-200 px-7 dark:bg-slate-900'>
+    <footer className='relative py-3 transform translate-y-full bg-gray-200 px-7'>
       <div className='mx-auto max-w-screen-2xl'>
         <div className='flex justify-between mb-1'>
-          <p className='text-xs text-gray-500 dark:text-gray-300'>
+          <p className='text-xs text-gray-500'>
             Copyright © jeong922. All Rights Reserved.
           </p>
           <a
             href='https://github.com/jeong922'
-            className='flex items-center text-gray-600 hover:text-blue-900 dark:hover:text-blue-500 dark:text-gray-300'
+            className='flex items-center text-gray-600 hover:text-mainColor'
           >
             <GithubIcon />
           </a>
         </div>
         <a
-          href='https://github.com/jeong922/next.js_blog'
-          className='flex items-center text-xs text-gray-500 hover:text-blue-900 dark:hover:text-blue-500 dark:text-gray-300'
+          href='https://github.com/jeong922/nextjs_shoppy'
+          className='flex items-center text-xs text-gray-500 hover:text-mainColor'
         >
           <GithubIcon />
           <span className='ml-2'>
