640ef795ca3d2359678e60e98322930ff647db8d
jeong922 <hwajeong922@naver.com>
2023-08-07 16:01:37 +0900

    탑 버튼 수정

--- src/components/TopButton.tsx
+++ src/components/TopButton.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ArrowTopIcon from './icon/ArrowTopIcon';
 
 export default function TopButton() {
+  const [showMenu, setShowMenu] = useState(false);
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -11,12 +12,27 @@ export default function TopButton() {
     });
   };
 
+  const getScrollState = () => {
+    window.scrollY >= 100 ? setShowMenu(true) : setShowMenu(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', getScrollState);
+    return () => {
+      window.removeEventListener('scroll', getScrollState);
+    };
+  }, []);
+
   return (
-    <button
-      onClick={scrollToTop}
-      className='sticky z-10 float-right p-2 bg-white border rounded-full border-neutral-300 right-8 bottom-8 text-neutral-600'
-    >
-      <ArrowTopIcon />
-    </button>
+    <>
+      {showMenu && (
+        <button
+          onClick={scrollToTop}
+          className='sticky z-10 float-right p-2 bg-white border rounded-full border-neutral-300 right-8 bottom-8 text-neutral-600'
+        >
+          <ArrowTopIcon />
+        </button>
+      )}
+    </>
   );
 }
